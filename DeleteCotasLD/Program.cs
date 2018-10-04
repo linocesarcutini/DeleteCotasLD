6a10828955f4908d4fe2c0791dd6d0215d3b572d
Lino Cesar Cutini <cesarcutini@gmail.com>
2018-10-04 11:18:58 -0300

    Add files via upload

--- DeleteCotasLD/Program.cs
+++ DeleteCotasLD/Program.cs
@@ -4,86 +4,83 @@ using System.Windows.Forms;
 using Autodesk.AutoCAD.Interop;
 using Autodesk.AutoCAD.Interop.Common;
 
-namespace ChangeTextInVariousDrawings
-{
-	class Program
-	{
+namespace ChangeTextInVariousDrawings {
+
+	class Program {
+
 		private static string[] arquivos;
 
 		[STAThread]
-		public static void Main(string[] args)
-		{
-			SelectFiles();
+		public static void Main(string[] args) {
 
-            AcadApplication acApp = null;
+			if (args.Length != 0) {
+				arquivos = args;
+			} else {
+				SelectFiles();
+			}
 
-            try
-            {
-                acApp = Marshal.GetActiveObject("AutoCAD.Application") as AcadApplication;
-            }
-            catch
-            {
-                MessageBox.Show("Não foi possível abrir o AutoCAD");
-            }
+			AcadApplication acApp = null;
+
+			try {
+				acApp = Marshal.GetActiveObject("AutoCAD.Application") as AcadApplication;
+			} catch {
+				MessageBox.Show("Não foi possível abrir o AutoCAD");
+			}
 			
-        	foreach (var desenho in arquivos)
-        	{
-        		AcadDocument doc;
+			foreach (var desenho in arquivos) {
+				AcadDocument doc;
 
-                try
-                {
-                    doc = acApp.Documents.Open(desenho, false, string.Empty);
-                    AcadModelSpace modelSpace = doc.ModelSpace;
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Não foi possível abrir o desenho {0}", desenho);
-                    break;
-                }
+				try {
+					//doc = acApp.Documents.Open(desenho, false, string.Empty);
+					doc = acApp.ActiveDocument.Open(desenho); //, false, string.Empty);
+					AcadModelSpace modelSpace = doc.ModelSpace;
+				} catch (Exception) {
+					MessageBox.Show("Não foi possível abrir o desenho " + desenho);
+					break;
+				}
 
-                AcadSelectionSet selset = null;
-                selset = doc.SelectionSets.Add("layer");
+				AcadSelectionSet selset = null;
+				selset = doc.SelectionSets.Add("layer");
 				short[] ftype = { 8 };
 				object[] fdata = { "COTAS,L.D." };
-                selset.Select(AcSelect.acSelectionSetAll, null, null, ftype, fdata);
+				selset.Select(AcSelect.acSelectionSetAll, null, null, ftype, fdata);
 
-                if (selset.Count != 0) {
-
-                	selset.Erase();
-                	doc.PurgeAll();
-                	doc.PurgeAll();
-                	doc.PurgeAll();
+				if (selset.Count != 0) {
+					selset.Erase();
+					doc.PurgeAll();
+					doc.PurgeAll();
+					doc.PurgeAll();
 					acApp.ZoomExtents();
 					doc.Save();
 					doc.Close();
 
-                } else {
-
-                	doc.Close();
-
-                }
-        	}
+				} else {
+					doc.PurgeAll();
+					doc.PurgeAll();
+					doc.PurgeAll();
+					acApp.ZoomExtents();
+					doc.Save();
+					doc.Close();
+				}
+			}
 		}
 		
-        static void SelectFiles()
-        {
-            // Displays an OpenFileDialog so the user can select a Cursor.
-            OpenFileDialog openFileDialog2 = new OpenFileDialog();
-            openFileDialog2.Filter = "Drawing Files|*.dwg";
-            openFileDialog2.Title = "Selecione os arquivos DWG";
-            openFileDialog2.RestoreDirectory = true;
-            openFileDialog2.Multiselect = true;
+		static void SelectFiles() {
 
-            if (openFileDialog2.ShowDialog() == DialogResult.OK)
-            {
-                // Guarda a lista dos caminhos completos dos arquivos selecionados
-                arquivos = openFileDialog2.FileNames;
-            }
-            else 
-            {
+			// Displays an OpenFileDialog so the user can select a Cursor.
+			OpenFileDialog openFileDialog2 = new OpenFileDialog();
+			openFileDialog2.Filter = "Drawing Files|*.dwg";
+			openFileDialog2.Title = "Selecione os arquivos DWG";
+			openFileDialog2.RestoreDirectory = true;
+			openFileDialog2.Multiselect = true;
+
+			if (openFileDialog2.ShowDialog() == DialogResult.OK) {
+				// Guarda a lista dos caminhos completos dos arquivos selecionados
+				arquivos = openFileDialog2.FileNames;
+			} else {
 				Console.WriteLine("Arquivos não selecionados!");
 				Console.ReadKey();
-            }
-        }
+			}
+		}
 	}
 }
