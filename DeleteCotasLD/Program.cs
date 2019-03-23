2a440f7b16994e28970969c00df44cd4787e30cb
Lino Cesar Cutini <lino.cc@brametal.com.br>
2019-03-23 09:13:25 -0300

    Agregado varios novos Layers

--- DeleteCotasLD/Program.cs
+++ DeleteCotasLD/Program.cs
@@ -42,7 +42,7 @@ namespace ChangeTextInVariousDrawings
                 try
                 {
                     doc = acApp.Documents.Open(desenho, false, string.Empty);
-                    //doc = acApp.ActiveDocument.Open(desenho); //, false, string.Empty);
+
                     AcadModelSpace modelSpace = doc.ModelSpace;
                 }
                 catch (Exception ex)
@@ -51,44 +51,43 @@ namespace ChangeTextInVariousDrawings
                     break;
                 }
 
-                //doc.SendCommand("(COMMAND \"SETVAR\" \"CLAYER\" \"0\")");
                 doc.SetVariable("CLAYER", "0");
 
                 AcadSelectionSet selset = null;
                 selset = doc.SelectionSets.Add("layer");
                 short[] ftype = { 8 };
-                object[] fdata = {
-                    "COTAS,L.D.," +
-                    "L1,L2,L3,L4,L5," +
-                    "REV.L1,REV.L2,REV.L3,REV.L4,REV.L5," +
-                    "00L1,00L2,00L3,00L4,00L5," +
-                    "R00L1,R00L2,R00L3,R00L4,R00L5," +
-                    "0AL1,0AL2,0AL3,0AL4,0AL5," +
-                    "01L1,01L2,01L3,01L4,01L5" +
-                    "A1L1,A1L2,A1L3,A1L4,A1L5" };
+                string[] fdataX = {
+                    "COTAS", "L.D.", "L1", "L2 ", "L3", "L4", "L5",
+                    "REV.L1", "REV.L2", "REV.L3","REV.L4","REV.L5",
+                    "00L1","00L2","00L3","00L4","00L5",
+                    "R00L1","R00L2","R00L3","R00L4","R00L5",
+                    "0AL1","0AL2","0AL3","0AL4","0AL5",
+                    "0BL1","0BL2","0BL3","0BL4","0BL5",
+                    "0CL1","0CL2","0CL3","0CL4","0CL5",
+                    "01L1","01L2","01L3","01L4","01L5",
+                    "02L1","02L2","02L3","02L4","02L5",
+                    "R01L1","R01L2","R01L3","R01L4","R01L5",
+                    "A1L1","A1L2","A1L3","A1L4","A1L5", "RL01",
+                    "RL001", "R0011", "RL2", "RL3", "RL4"};
 
-                selset.Select(AcSelect.acSelectionSetAll, null, null, ftype, fdata);
-
-                if (selset.Count != 0)
+                foreach (var item in fdataX)
                 {
-                    selset.Erase();
-                    doc.PurgeAll();
-                    doc.PurgeAll();
-                    doc.PurgeAll();
-                    acApp.ZoomExtents();
-                    doc.Save();
-                    doc.Close();
+                    object[] fdata = { item };
 
+                    selset.Select(AcSelect.acSelectionSetAll, null, null, ftype, fdata);
+
+                    if (selset.Count != 0)
+                    {
+                        selset.Erase();
+                    }
                 }
-                else
-                {
-                    doc.PurgeAll();
-                    doc.PurgeAll();
-                    doc.PurgeAll();
-                    acApp.ZoomExtents();
-                    doc.Save();
-                    doc.Close();
-                }
+
+                doc.PurgeAll();
+                doc.PurgeAll();
+                doc.PurgeAll();
+                acApp.ZoomExtents();
+                doc.Save();
+                doc.Close();
             }
         }
 
